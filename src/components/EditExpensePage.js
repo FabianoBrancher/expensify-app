e0a5e17df418ea1c7dbc02f573b1507bbae32d5d
fabianobrancher <fabiano.brancher@hotmail.com>
2018-03-04 14:57:08 -0300

    Testing ExpenseListFilters Part I and II

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,33 +3,68 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense} from '../actions/expenses';
 
-const EditExpensePage = (props) => {
-    // console.log(props);
-    return (
-        <div>
-            <ExpenseForm
-                expense={props.expense}
-                onSubmit={ (expense) => {
-                    console.log('updated', expense);
-                    props.dispatch(editExpense(props.expense.id, expense));
-                    props.history.push('/');
-                }}
-            />
-            <button onClick={() => {
-                console.log(props.expense.id);
-                props.dispatch(removeExpense( { id: props.expense.id }));
-                props.history.push('/');
-                }}
-            >Remove 
-            </button>
-        </div>  
-    );
+export class EditExpensePage extends React.Component {
+    onSubmit = (expense) => {
+        console.log('updated', expense);
+        this.props.editExpense(this.props.expense.id, expense);
+        this.props.history.push('/');
+    };
+    onRemove = () => {
+        console.log(this.props.expense.id);
+        this.props.removeExpense( { id: this.props.expense.id });
+        this.props.history.push('/');
+    };
+    render () {
+        return (
+            <div>
+                <ExpenseForm
+                    expense={this.props.expense}
+                    onSubmit={this.onSubmit}
+                />
+                <button 
+                    onClick={this.onRemove}
+                >Remove 
+                </button>
+            </div>  
+        );      
+    }
 };
 
 const mapStateToProps = (state, props) => {
     return {
         expense: state.expenses.find((expense) => expense.id === props.match.params.id)
     };
-};  
+};
+const mapDispatchToProps = (dispatch, props) => {
+    return {
+        editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+        removeExpense: (data) => dispatch(removeExpense(data))
+    };
+};
 
-export default connect(mapStateToProps)(EditExpensePage);
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
+
+
+
+// const EditExpensePage = (props) => {
+//     // console.log(props);
+//     return (
+//         <div>
+//             <ExpenseForm
+//                 expense={props.expense}
+//                 onSubmit={ (expense) => {
+//                     console.log('updated', expense);
+//                     props.dispatch(editExpense(props.expense.id, expense));
+//                     props.history.push('/');
+//                 }}
+//             />
+//             <button onClick={() => {
+//                 console.log(props.expense.id);
+//                 props.dispatch(removeExpense( { id: props.expense.id }));
+//                 props.history.push('/');
+//                 }}
+//             >Remove 
+//             </button>
+//         </div>  
+//     );
+// };
